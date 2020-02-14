fa3056bce21e9ecb33b3e5a1ee4c153c91ebfda6
sachix1001 <sachix1001@gmail.com>
2020-02-14 11:13:49 +0900

    update allData

--- seeds/genre.js
+++ seeds/genre.js
@@ -1,24 +1,24 @@
-let allData = require('../data/allData.json')
-allData = allData.map(data =>{
-  return {
-    "movie_id": data.movie_id,
-    "genres": data.genres,
-    "imdb_id": data.imdb_id,
-    "popularity": data.popularity,
-    "overview": data.overview,
-    "title": data.title,
-    "poster": data.poster,
-    "keywords": data.keywords
-  }
-})
-
+let allData = require("../data/allData.json");
+allData = allData
+  .map(data => {
+    return {
+      movie_id: data.movie_id,
+      genres: data.genres,
+      imdb_id: data.imdb_id,
+      popularity: data.popularity,
+      overview: data.overview,
+      title: data.title,
+      poster: data.poster,
+      keywords: data.keywords
+    };
+  });
 
 exports.seed = function(knex) {
-   // Deletes ALL existing entries
+  // Deletes ALL existing entries
   return knex("movies")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("movies").insert(allData.slice(0,1000));
+      return knex("movies").insert(allData);
     });
 };
